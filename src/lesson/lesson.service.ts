174b56381a53eced4aec710193067274943b7b55
Jodi Dublon <jodi.dublon@thedistance.co.uk>
2021-07-01 14:52:43 +0100

    add get all students and get student by id

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -30,4 +30,10 @@ export class LessonService {
   async getLesson(id): Promise<Lesson> {
     return this.lessonRepository.findOne({id})
   }
+
+  async assignStudentsToLesson(lessonId: string, studentIds: string[]): Promise<Lesson> {
+    const lesson = await this.lessonRepository.findOne({ id: lessonId });
+    lesson.students = [...lesson.students, ...studentIds];
+    return this.lessonRepository.save(lesson);
+  }
 }
